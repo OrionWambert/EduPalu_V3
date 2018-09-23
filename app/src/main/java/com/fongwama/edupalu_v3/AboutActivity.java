96118436c91d9d1815b088ff0bf3d81d75aab1c9
king-peter <petenet001@gmai.com>
2018-09-23 18:01:04 +0200

    mise en place etv finalisation de la page about

--- app/src/main/java/com/fongwama/edupalu_v3/AboutActivity.java
+++ app/src/main/java/com/fongwama/edupalu_v3/AboutActivity.java
@@ -2,6 +2,7 @@ package com.fongwama.edupalu_v3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class AboutActivity extends AppCompatActivity {
 
--- app/src/main/java/com/fongwama/edupalu_v3/MainActivity.java
+++ app/src/main/java/com/fongwama/edupalu_v3/MainActivity.java
@@ -1,5 +1,6 @@
 package com.fongwama.edupalu_v3;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -77,7 +78,9 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_about) {
+            Intent i = new Intent(MainActivity.this, AboutActivity.class);
+            startActivity(i);
             return true;
         }
 
